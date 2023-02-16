b66123f1adb457e93318ff74ad639410b7cd5dd3
rexsimiloluwah <adetoyosi2001@gmail.com>
2023-02-16 05:18:59 +0100

    fix: opened ssh connection in the server security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,6 +98,14 @@ resource "aws_security_group" "ec2_server_allow_web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
